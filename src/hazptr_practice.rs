f349b3839c6eb7290d1b5e5192c0f7c6d0984fae
Felix Prasanna <felix725@gmail.com>
2022-04-26 13:01:14 -0400

    added more

--- src/hazptr_practice.rs
+++ src/hazptr_practice.rs
@@ -3,7 +3,7 @@ extern crate alloc;
 use alloc::boxed::Box;
 use core::{marker::PhantomData, sync::atomic::AtomicPtr};
 
-use haphazard::{self, raw::Pointer};
+use haphazard::{self, raw::Pointer, Singleton};
 
 // Setting up hazard pointers
 // This makes sure they all use the same Domain, guaranteeing the protection is valid.
@@ -16,31 +16,60 @@ type HazAtomicPtr<T> = haphazard::AtomicPtr<T, Family>;
 struct DataPtr<T> {
     data: HazAtomicPtr<T>,
     domain: Domain,
-    _marker: PhantomData<T>
+    _marker: PhantomData<T>,
 }
 
-struct Data (usize, usize);
+struct Data(usize, usize);
 
-impl<T> DataPtr<T> where T: Copy {
+impl<T> DataPtr<T>
+where
+    T: Copy,
+{
     fn new(data: T) -> Self {
         Self {
+            // this is safe because the ptr comes from into_raw
             data: unsafe { HazAtomicPtr::new(Box::new(data).into_raw()) },
             domain: Domain::new(&Family {}),
-            _marker: PhantomData::<T>
-
+            _marker: PhantomData::<T>,
         }
     }
 
-    fn load() -> T {
+    fn load(&self) -> T {
         todo!()
+        let mut hp = HazardPointer::new_in_domain(&self.domain);
     }
 
-    fn store(data: T) {
+    fn store(&self, data: T) {
         // just a simple cas loop
-        todo!()
+        // # SAFETY
+        // this is safe because the ptr comes from into_raw
+        let new = Box::new(data).into_raw();
+        loop {
+            // # SAFETY
+            // this is safe because all hazptrs and loads are using the domain carried by the struct
+            // The unwrap is safe because the pointer being derefed comes from Box::into_raw
+
+            // # SAFETY
+            // This is safe because `new` comes from Box::into_raw
+            let s = unsafe {
+                HazAtomicPtr::compare_exchange_weak_ptr(&self.data, self.data.load_ptr(), new)
+            };
+            match s {
+                Ok(ptr) => {
+                    if let Some(ptr) = ptr {
+                        // # Safety
+                        unsafe { ptr.retire_in(&self.domain) };
+                        break
+                    } else {
+                        // None case, not sure when this would happen if cmpxchg is successful
+                        continue;
+                    }
+                }
+                Err(_) => continue,
+            }
+        }
     }
 }
-
 
 fn run_it() {
     todo!()
